cf74f3240cf9d559da76240a3a056de604cfe0d9
Utkarsh Tripathi <shandilya0103@gmail.com>
2025-05-12 17:59:18 +0530

    Add project files.

---
+++ CitiesManager.WebAPI/Migrations/20250512114431_Initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CitiesManager.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    CityID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.CityID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityID", "CityName" },
+                values: new object[,]
+                {
+                    { new Guid("6738c94d-bdea-4889-ba64-9b327bca7766"), "Lucknow" },
+                    { new Guid("b4f121ab-0c63-4567-935a-9aba64ee99aa"), "Banaras" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
---
+++ CitiesManager.WebAPI/Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace CitiesManager.WebAPI.Models
+{
+    public class City
+    {
+        public Guid? CityID { get; set; }
+        public string CityName { get; set; }
+    }
+}
